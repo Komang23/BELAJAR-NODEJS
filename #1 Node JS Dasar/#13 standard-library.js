84a0dd1da26984dd2e85414e9a69d76f9fe5cb75
JKS <komangredana13@gmail.com>
2024-08-24 13:57:29 +0700

    HARI KE 1 BELAJAR NODE JS

---
+++ #1 Node JS Dasar/#13 standard-library.js
@@ -0,0 +1,6 @@
+import os from "os";
+ 
+console.info(os.platform());
+console.table(os.cpus());
+
+// eror karena bukan js moduls file 
---
+++ #1 Node JS Dasar/#15 async.js
@@ -0,0 +1,9 @@
+function samplePromise(){
+    return Promise.resolve("eko");
+}
+
+async function run() {
+    const name = await samplePromise();
+    console.info(name)
+}
+run();
